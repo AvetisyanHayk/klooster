a36bdcac6772512c208828baf1a982963f2c9068
AvetisyanHayk <hayk.avetisyan@student.howest.be>
2017-06-04 11:37:32 +0200

    GedachteComparatorOpBasisVanCreativiteit - Beta - fix

--- src/be/howest/klooster/core/GedachteComparatorOpBasisVanCreativiteit.java
+++ src/be/howest/klooster/core/GedachteComparatorOpBasisVanCreativiteit.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package be.howest.klooster.core;
 
 import java.io.Serializable;
@@ -16,7 +11,7 @@ public class GedachteComparatorOpBasisVanCreativiteit
         implements Comparator<Gedachte>, Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
     @Override
     public int compare(Gedachte gedachte1, Gedachte gedachte2) {
         if (gedachte1 == null && gedachte2 == null) {
@@ -30,5 +25,4 @@ public class GedachteComparatorOpBasisVanCreativiteit
         }
         return gedachte1.getCreativiteit() - gedachte2.getCreativiteit();
     }
-
 }
