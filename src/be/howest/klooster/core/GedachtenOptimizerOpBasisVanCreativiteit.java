c2c8f9be6b8be188a90695497f94ec999cab7034
AvetisyanHayk <hayk.avetisyan@student.howest.be>
2017-06-04 15:08:26 +0200

    GedachteOptimizerOpBasisVanCreativiteit - Beta

--- src/be/howest/klooster/core/GedachtenOptimizerOpBasisVanCreativiteit.java
+++ src/be/howest/klooster/core/GedachtenOptimizerOpBasisVanCreativiteit.java
@@ -1,6 +1,6 @@
 package be.howest.klooster.core;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -28,28 +28,25 @@ public class GedachtenOptimizerOpBasisVanCreativiteit
         Map<Integer, List<Gedachte>> gedachtenMap
                 = GedachtenOptimizer.getGedachtenMap(pater.getGedachten());
         Set<Gedachte> gedachtenSet = getGeoptimaliseerdeGedachten(gedachtenMap,
-                pater.getGoedheid());
+                pater.getCreativiteit());
         pater.setGedachten(gedachtenSet.toArray(new Gedachte[Pater.MAX_GEDACHTEN]));
         return aantalGedachten - gedachtenSet.size();
     }
 
-    private Set<Gedachte> getGeoptimaliseerdeGedachten(
-            Map<Integer, List<Gedachte>> gedachtenMap, int goedheid) {
+    private Set<Gedachte> getGeoptimaliseerdeGedachten(Map<Integer, List<Gedachte>> gedachtenMap, int creativiteit) {
         Set<Gedachte> geoptimaliseerdeGedachten = new LinkedHashSet<>();
         for (Map.Entry<Integer, List<Gedachte>> entry : gedachtenMap.entrySet()) {
             List<Gedachte> gedachtenList = entry.getValue();
-            Gedachte gedachteMetHoogsteCreativiteit
-                    = getGedachteMetHoogsteCreativiteit(gedachtenList);
-            geoptimaliseerdeGedachten.add(gedachteMetHoogsteCreativiteit);
+            Gedachte gedachteDichtsBijCreativiteitVanPater
+                    = getGedachteMetHoogsteCreativiteit(gedachtenList, creativiteit);
+            geoptimaliseerdeGedachten.add(gedachteDichtsBijCreativiteitVanPater);
         }
         return geoptimaliseerdeGedachten;
     }
 
-    private Gedachte getGedachteMetHoogsteCreativiteit(
-            List<Gedachte> gedachtenSet) {
-        Gedachte[] gedachten = gedachtenSet
-                .toArray(new Gedachte[gedachtenSet.size()]);
-        Arrays.sort(gedachten, new GedachteComparatorOpBasisVanCreativiteit());
-        return gedachten[0];
+    private Gedachte getGedachteMetHoogsteCreativiteit(List<Gedachte> gedachten, int creativiteit) {
+        Collections.sort(gedachten,
+                new GedachteComparatorOpBasisVanCreativiteit(creativiteit));
+        return gedachten.get(0);
     }
 }
